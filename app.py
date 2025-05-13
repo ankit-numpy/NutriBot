87cbff3b50e27cd3ad2bb9e67468a6f9564071f5
Ankit Dixit <88057272+ankit-numpy@users.noreply.github.com>
2025-05-13 11:53:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ genai.configure(api_key="AIzaSyAuWjWVjVM-jGsoFy-hp8xr2JL97-_aekc")
 ## Function to load Google Gemini Pro Vision API And get response
 
 def get_gemini_repsonse(input,image,prompt):
-    model=genai.GenerativeModel('gemini-pro-vision')
+    model=genai.GenerativeModel('gemini-2.0-flash')
     response=model.generate_content([input,image[0],prompt])
     return response.text
 
