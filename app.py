e313f16f08d15015ea3bf722206ddb6b66f4f4ed
Ankit Dixit <88057272+ankit-numpy@users.noreply.github.com>
2025-05-13 11:48:07 +0530

    Update app.py
    direct api reference

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import os
 import google.generativeai as genai
 from PIL import Image
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+#genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key="your_actual_api_key_here")
 
 ## Function to load Google Gemini Pro Vision API And get response
 
